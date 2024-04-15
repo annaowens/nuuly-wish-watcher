ba504684079622ca19f82fc82ad2784eec55f075
Anna Owens <aowens@miraclemileadvisors.com>
2024-04-15 07:23:04 -0700

    fix routing

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,18 +1,13 @@
 // App.tsx
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import NavBar from './components/navBar';
 import LandingPage from './components/landingPage';
-import FAQPage from './components/faqPage'; // Assuming you have a FAQ page component
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      {/* <NavBar /> */}
-      <Route path="/" exact component={LandingPage} />
-      <Route path="/faq" component={FAQPage} />
-    </Router>
+    <div>
+      <LandingPage />
+    </div>
   );
 };
 
